8d42c656ea1827013d3a0b1add0c1ff2ed38afa1
G. Ferguson <8si.greg@gmail.com>
2010-03-04 09:35:55 -0700

    initial add

---
+++ ruby/craigslist_jobs.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby -w
+
+require 'rubygems'
+require 'mechanize'
+require 'nokogiri'
+require 'uri'
+require 'rss'
+require 'set'
+
+JOBS_URL    = 'http://phoenix.craigslist.org/sof/index.html'
+HREF_SUBSTR = %r{phoenix\.craigslist\.org/evl/sof/}
+JOB_REGEX   = %r/\b(?:perl|ruby|sql|python|postgres)\b/i
+
+mech = Mechanize.new
+
+mech.get(JOBS_URL)
+
+content = RSS::Maker.make('2.0') do |_feed|
+  _feed.channel.title       = "Craig's List jobs"
+  _feed.channel.link        = JOBS_URL
+  _feed.channel.description = "Software jobs of interest at Craig's List"
+  _feed.items.do_sort       = true # sort items by date
+
+  mech.page.links_with(:href => HREF_SUBSTR).each do |_link|
+
+    mech.get(_link.href)
+    response_body = Nokogiri::HTML(mech.page.content).inner_text
+
+    hits = response_body.split(' ').select{ |w| w[JOB_REGEX] }.to_set
+    if (hits.any?)
+      post_date = response_body[/Date:\s+(\S+\s+\S+\s+\S+)/i, 1]
+      i = _feed.items.new_item
+      i.date        = Time.parse(post_date)
+      i.link        = _link.href
+      i.title       = mech.page.at('h2').text.squeeze(' ').strip
+      i.description = 'Matching words: ' << hits.to_a.join(', ')
+    end
+  end
+end
+
+puts "Content-type: application/rss+xml"
+puts
+puts content, "\n"
+
